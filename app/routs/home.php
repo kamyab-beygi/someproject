9fb91d422bd402ea8046196806b191c57d002a81
Kamyab Alex <kamyab.ali.b@gmail.com>
2020-03-05 03:23:22 +0100

    route done

--- app/routs/home.php
+++ app/routs/home.php
@@ -13,4 +13,14 @@ $app->get('/flash',function() use($app){
 	$app->response->redirect($app->urlFor('home'));
 });
 
+
+
+
+$app->get('/flash2',function() use($app){
+
+	$app->flash('global',"yes FUck aLL");
+
+	$app->response->redirect($app->urlFor('home'));
+});
+
 ?>
